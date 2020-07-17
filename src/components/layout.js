4492b7c9167d90cd09eeabfb06b23e72c6858b0a
Jen Chow <jennego@gmail.com>
2020-07-16 21:55:33 -0700

    Set up project

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,14 +26,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div>
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,22 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="jumbotron jumbotron-fluid hero">
+      <div className="container">
+        <h1 className="display-4">Jennego</h1>
+        <p> This is my hobby site. Eventually full screen video overlay? </p>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <div className="container">
+      <p> Hi this is my hobby site! </p>
+      <p> Need a pull out sidebar or toggle menu button</p>
+      <p> Dark dark purple theme </p>
+      <p> Prismic for CMS? </p>
+      <p> WP for blog?</p>
+      <p> Use Sass? </p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </div>
   </Layout>
 )
 
