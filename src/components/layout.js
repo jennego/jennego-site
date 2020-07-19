c4dc9c94df61fc0616a1e2e342d6da9e3d9a7a06
Jen Chow <jennego@gmail.com>
2020-07-18 23:35:01 -0700

    Add sass. Changed file structure to scss.

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "bootstrap/dist/css/bootstrap.min.css"
-import "./layout.css"
+import "../styles/styles.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
