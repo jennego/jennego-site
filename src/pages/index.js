d11c54000c5fae1d53e7a3cd3a4737a805b4ea53
Jen Chow <jennego@gmail.com>
2020-07-22 21:40:32 -0700

    change front image to gatsby image, add gatsby background image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,54 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import Nav from "../components/menu"
+import Img from "gatsby-image"
+import BackgroundImage from "gatsby-background-image"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div className="hero">
-      <div className="overlay">
-        <div className="container">
-          <h1 className="display-4">Jennego</h1>
-          <p> This is my hobby site. Eventually full screen video overlay? </p>
-          <br></br>
-          <p> Need a pull out sidebar or toggle menu button</p>
-          <p> Dark dark purple theme </p>
-          <p> WP for blog?</p>
-          <p> Use Sass </p>
-          <p> Contentful </p>
-          <Link to="/page-2/">Go to page 2</Link> <br />
-          <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-        </div>
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    {
+      file(name: { eq: "backyardsky" }) {
+        name
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid
+          }
+          fixed {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div className="hero">
+        <BackgroundImage fluid={data.file.childImageSharp.fluid}>
+          <div className="overlay">
+            <div className="container hero-text">
+              <h1 className="display-4">Jennego</h1>
+              <p>
+                {" "}
+                This is my hobby site. Eventually full screen video overlay?{" "}
+              </p>
+              <br></br>
+              <p> Need a pull out sidebar or toggle menu button</p>
+              <p> Dark dark purple theme </p>
+              <p> WP for blog?</p>
+              <p> Use Sass </p>
+              <p> Contentful </p>
+              <Link to="/page-2/">Go to page 2</Link> <br />
+              <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+            </div>
+          </div>
+        </BackgroundImage>
       </div>
-    </div>
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 export default IndexPage
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,39 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Img from "gatsby-image"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = () => {
+  const data = useStaticQuery(graphql`
+    {
+      file(name: { eq: "backyardsky" }) {
+        name
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+          fixed {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Layout>
+      <SEO title="Page two" />
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+      <Link to="/">Go back to the homepage</Link>
+      <div style={{ maxWidth: "800px" }}>
+        <Img className="img-fluid" fluid={data.file.childImageSharp.fluid} />
+      </div>
+      <h2>Fixed</h2>
+      <Img fixed={data.file.childImageSharp.fixed} />
+    </Layout>
+  )
+}
 
 export default SecondPage
