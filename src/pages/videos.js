a96455c9c71d57994c245125d1be59bacf846000
Jennifer Chow <jennego@gmail.com>
2020-11-18 23:32:41 -0800

    add layout to some pages

--- src/pages/videos.js
+++ src/pages/videos.js
@@ -1,12 +1,15 @@
 import React from "react"
 import SimpleReactLightbox from "simple-react-lightbox"
+import Layout from "../components/layout"
 
 const VideoPage = () => {
   return (
-    <div className="container-fluid">
-      <h1>Videos</h1>
-      <SimpleReactLightbox></SimpleReactLightbox>
-    </div>
+    <Layout>
+      <div className="container-fluid">
+        <h1>Videos</h1>
+        <SimpleReactLightbox></SimpleReactLightbox>
+      </div>
+    </Layout>
   )
 }
 
--- src/pages/web.js
+++ src/pages/web.js
@@ -1,13 +1,16 @@
 import React from "react"
+import Layout from "../components/layout"
 
 const WebPage = () => {
   return (
-    <div className="container-fluid">
-      <h1>Web Things</h1>
-      <p>Some fun, decidedly non-professional things I've made!</p>
-      <h2>Hangman</h2>
-      <h2>Tic Tac Whoa</h2>
-    </div>
+    <Layout>
+      <div className="container-fluid">
+        <h1>Web Things</h1>
+        <p>Some fun, decidedly non-professional things I've made!</p>
+        <h2>Hangman</h2>
+        <h2>Tic Tac Whoa</h2>
+      </div>
+    </Layout>
   )
 }
 
