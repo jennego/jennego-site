2e403ce16b3b671bf0984e0f25325d5d8ca8cced
Jennifer Chow <jennego@gmail.com>
2020-12-16 23:45:04 -0800

    add social media, add social media fontawesome

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,16 @@
 import React from "react"
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faFacebook,
+  faInstagram,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons"
 const Footer = () => (
   <footer>
     <div className="container-sm">
       <div className="row footer">
         <div className="col">
-          <p> Jennego </p>
+          <p className="display-4"> Jennego </p>
         </div>
         <div className="col">
           <ul class="footer-menu">
@@ -13,9 +18,20 @@ const Footer = () => (
             <li> Photos </li>
             <li> Videos </li>
             <li> Web Things </li>
+            <li> Blog </li>
           </ul>
         </div>
-        <div className="col">Social Media </div>
+        <div className="col">
+          <a href="https://www.facebook.com/jennego" target="_open">
+            <FontAwesomeIcon icon={faFacebook} size="3x" className="mr-2" />
+          </a>
+          <a href="https://www.instagram.com/thejennego" target="_open">
+            <FontAwesomeIcon icon={faInstagram} size="3x" className="mr-2" />
+          </a>
+          <a href="https://github.com/jennego" target="_open">
+            <FontAwesomeIcon icon={faGithub} size="3x" className="mr-2" />
+          </a>
+        </div>
       </div>
       <div className="row footer">
         © {new Date().getFullYear()}, Built with
