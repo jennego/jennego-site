b12c963bd2aa825429abeeba4df6b61ea066a43e
Jennifer Chow <jennego@gmail.com>
2021-01-20 23:05:23 -0800

    Tidy scss at bit. Need to fix modal like I did for portfolio.

--- src/pages/videos.js
+++ src/pages/videos.js
@@ -4,7 +4,7 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap"
 import { useStaticQuery, graphql } from "gatsby"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faPlayCircle } from "@fortawesome/free-regular-svg-icons"
-
+import ResponsiveEmbed from "react-responsive-embed"
 const VideoPage = props => {
   const data = useStaticQuery(graphql`
     {
@@ -21,7 +21,6 @@ const VideoPage = props => {
       }
     }
   `)
-  const { buttonLabel, className } = props
   const [modal, setModal] = useState(false)
   const toggle = () => setModal(!modal)
 
@@ -32,49 +31,38 @@ const VideoPage = props => {
         {console.log(data)}
         <div className="row no-gutters">
           {data.allContentfulVideo.edges.map(({ node }) => (
-            <div className="col-4">
-              <div className="card video-thumb bg-dark border-0">
-                <img className="card-img" src={node.thumbUrl} />
-                <div class="d-flex card-img-overlay thumb-overlay justify-content-center align-items-center">
-                  <FontAwesomeIcon
-                    icon={faPlayCircle}
-                    size="8x"
-                    color="rgba(255, 255, 255, 0.5)"
-                    className="play-icon"
-                  />
+            <>
+              <div className="col-4">
+                <div
+                  className="card video-thumb bg-dark border-0"
+                  onClick={toggle}
+                  onKeyDown={toggle}
+                >
+                  <img className="card-img" src={node.thumbUrl} />
+                  <div class="d-flex card-img-overlay thumb-overlay justify-content-center align-items-center">
+                    <FontAwesomeIcon
+                      icon={faPlayCircle}
+                      size="8x"
+                      color="rgba(45, 139, 247, 0)"
+                      className="play-icon"
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
+
+              <Modal isOpen={modal} toggle={toggle} className="modal-lg">
+                <ModalHeader toggle={toggle}>
+                  <h2
+                    dangerouslySetInnerHTML={{ __html: `${node.title}` }}
+                  ></h2>
+                </ModalHeader>
+                <ModalBody>
+                  <ResponsiveEmbed src={node.videoUrl} allowFullScreen />
+                </ModalBody>
+                <ModalFooter></ModalFooter>
+              </Modal>
+            </>
           ))}
-        </div>
-        <div>
-          <Button color="danger" onClick={toggle}>
-            HI
-          </Button>
-          <Modal isOpen={modal} toggle={toggle} className={className}>
-            <ModalHeader toggle={toggle} className="text-dark">
-              Modal title
-            </ModalHeader>
-            <ModalBody>
-              <p className="text-dark">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-            </ModalBody>
-            <ModalFooter>
-              <Button color="primary" onClick={toggle}>
-                Do Something
-              </Button>
-              <Button color="secondary" onClick={toggle}>
-                Cancel
-              </Button>
-            </ModalFooter>
-          </Modal>
         </div>
       </div>
     </Layout>
