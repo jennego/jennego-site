53e999977304af743e1097077f30f777495ddcff
Jennifer Chow <jennego@gmail.com>
2021-03-21 23:31:07 -0700

    Fix front page. Transition to gatsby static image.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,32 @@
 import React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import { GatsbyImage } from "gatsby-plugin-image";
-import BackgroundImage from "gatsby-background-image"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
-  const data = useStaticQuery(graphql`{
-  file(name: {eq: "backyardsky"}) {
-    name
-    childImageSharp {
-      gatsbyImageData(quality: 90, layout: FULL_WIDTH)
-      fixed {
-        ...GatsbyImageSharpFixed
-      }
-    }
-  }
-}
-`)
   return (
     <Layout>
       <SEO title="Home" />
       <div className="hero">
-        <BackgroundImage fluid={data.file.childImageSharp.gatsbyImageData}>
-          <div className="overlay">
-            <div className="container hero-text">
-              <h1 className="display-1">Jennego</h1>
-              <h2>Photography. Writing. Web Experiments. And other things!</h2>
-            </div>
+        <StaticImage
+          alt="hi"
+          src="../images/backyardsky.jpg"
+          placeholder="blurred"
+          className="index-image-wrapper"
+          imgClassName="index-image"
+        />
+        <div className="overlay">
+          <div className="container hero-text">
+            <h1 className="display-1">Jennego</h1>
+            <h2>Photography. Writing. Web Experiments. And other things!</h2>
           </div>
-        </BackgroundImage>
+        </div>
       </div>
     </Layout>
-  );
+  )
 }
 
 export default IndexPage
