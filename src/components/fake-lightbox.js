026f209b6019d43d8414ee22a920e34604d0337b
Jennifer Chow <jennego@gmail.com>
2021-04-24 14:12:29 -0700

    clicking thumbmails work

--- src/components/fake-lightbox.js
+++ src/components/fake-lightbox.js
@@ -11,21 +11,35 @@ import AniLink from "gatsby-plugin-transition-link/AniLink"
 import { navigate } from "@reach/router"
 
 const FakeLightbox = (props, { fullImage }) => {
+  const [selectedImage, setSelectedImage] = useState(
+    props.data.contentfulPhotoGallery.gallery[1]
+  )
+  const [nextImage, setNextImage] = useState()
+  const [prevImage, setPrevImage] = useState()
+
   const handleUrlChange = () => {
-    window.history.pushState("page2 state", "Useless Title", "/photos")
-    console.log("hi")
+    // window.history.pushState("page2 state", "Useless Title", "/photos")
   }
-  console.log(props.data)
 
   // if props are not then use graphql query
   // take current id, affix it to thumbnail, scroll into view on load
+
+  const thumbnailSelectHandler = image => {
+    console.log(image)
+    setSelectedImage(image)
+  }
 
   return (
     <div className="fake-lightbox">
       <div className="row">
         <div className="col-2 d-md-block d-none thumbs">
           {props.data.contentfulPhotoGallery.gallery.map(photo => (
-            <GatsbyImage image={photo.gatsbyImageData} alt="hi" id={photo.id} />
+            <GatsbyImage
+              image={photo.gatsbyImageData}
+              alt="hi"
+              id={photo.id}
+              onClick={() => thumbnailSelectHandler(photo)}
+            />
           ))}
         </div>
 
@@ -40,21 +54,23 @@ const FakeLightbox = (props, { fullImage }) => {
         >
           <div
             className="photo-prev align-self-center "
-            style={{ position: "absolute", left: 0 }}
+            style={{ position: "absolute", left: 0, zIndex: "5" }}
           >
             <a onClick={handleUrlChange}>
               <FontAwesomeIcon icon={faAngleLeft} size="5x" />
             </a>
           </div>
           {/* feed props to image component which will change.   */}
-          <img
-            src="https://source.unsplash.com/random"
-            className="img-fluid"
+          <GatsbyImage
+            image={selectedImage.gatsbyImageData}
+            alt="hi"
+            id={selectedImage.id}
             style={{ height: "100vh", objectFit: "contain" }}
           />
+
           <div
             className="photo-next align-self-center "
-            style={{ position: "absolute", right: 0 }}
+            style={{ position: "absolute", right: 0, zIndex: "5" }}
           >
             <a onClick={handleUrlChange}>
               <FontAwesomeIcon icon={faAngleRight} size="5x" />
