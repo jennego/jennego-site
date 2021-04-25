d258c6841dbd10f5cf581d388ee33ecd5800b8ad
Jennifer Chow <jennego@gmail.com>
2021-04-24 23:20:17 -0700

    I think it's mostly working now....

--- src/components/fake-lightbox.js
+++ src/components/fake-lightbox.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { GatsbyImage } from "gatsby-plugin-image"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
   faAngleRight,
@@ -8,40 +9,84 @@ import {
 } from "@fortawesome/free-solid-svg-icons"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 import { navigate } from "@reach/router"
+import "animate.css/animate.min.css"
 
 const FakeLightbox = (props, { fullImage }) => {
+  const [selectedImage, setSelectedImage] = useState(
+    props.data.contentfulPhotoGallery.gallery[1]
+  )
+  const [nextImage, setNextImage] = useState()
+  const [prevImage, setPrevImage] = useState()
+  const [animateCss, setAnimateCss] = useState(
+    "animate__animated animate__fadeIn"
+  )
+
   const handleUrlChange = () => {
-    window.history.pushState("page2 state", "Useless Title", "/photos")
-    console.log("hi")
+    // window.history.pushState("page2 state", "Useless Title", "/photos")
+  }
+
+  // if props are not then use graphql query
+  // take current id, affix it to thumbnail, scroll into view on load
+
+  const thumbnailSelectHandler = image => {
+    console.log(image)
+    setSelectedImage(image)
+    setAnimateCss("animate__animated animate__fadeIn")
   }
 
   return (
     <div className="fake-lightbox">
       <div className="row">
-        <div className="col-1 thumbs">
-          {" "}
-          {/* hide on mobile */}
-          Thumbs
-        </div>
-        <div className="col-1 photo-prev">
-          <a onClick={handleUrlChange}>
-            <FontAwesomeIcon icon={faAngleLeft} size="5x" />
-          </a>
-        </div>
-
-        <div className="col main">
-          {/* feed props to image component which will change   */}
-          <img src="https://source.unsplash.com/random" className="img-fluid" />
-        </div>
-        <div className="col-1 photo-next">
-          <a onClick={handleUrlChange}>
-            <FontAwesomeIcon icon={faAngleRight} size="5x" />
-          </a>
+        <div className="col-2 d-md-block d-none thumbs">
+          {props.data.contentfulPhotoGallery.gallery.map(photo => (
+            <GatsbyImage
+              image={photo.gatsbyImageData}
+              alt="hi"
+              id={photo.id}
+              onClick={() => thumbnailSelectHandler(photo)}
+              tabIndex="0"
+            />
+          ))}
         </div>
 
-        <AniLink>
-          <FontAwesomeIcon icon={faTimes} size="5x" />
-        </AniLink>
+        <div
+          className="close"
+          style={{ position: "absolute", right: "5px", zIndex: "5" }}
+        >
+          <AniLink>
+            <FontAwesomeIcon icon={faTimes} size="lg" color="white" />
+          </AniLink>
+        </div>
+        <div
+          className="col-md-9 col-12 main d-flex flex-row justify-content-center"
+          style={{ background: "#3f364f" }}
+        >
+          <div
+            className="photo-prev align-self-center "
+            style={{ position: "absolute", left: 0, zIndex: "5" }}
+          >
+            <a onClick={handleUrlChange}>
+              <FontAwesomeIcon icon={faAngleLeft} size="5x" />
+            </a>
+          </div>
+          {/* feed props to image component which will change.   */}
+          <GatsbyImage
+            image={selectedImage.gatsbyImageData}
+            alt="hi"
+            id={selectedImage.id}
+            style={{ height: "100vh", objectFit: "contain" }}
+            className={animateCss}
+          />
+
+          <div
+            className="photo-next align-self-center "
+            style={{ position: "absolute", right: 0, zIndex: "5" }}
+          >
+            <a onClick={handleUrlChange}>
+              <FontAwesomeIcon icon={faAngleRight} size="5x" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   )
