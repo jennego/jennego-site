8bf88cc56bb1fb6d607f09cad357a83b683cdcad
Jennifer Chow <jennego@gmail.com>
2022-08-28 23:36:16 -0700

    add writing page index

--- src/pages/writing.js
+++ src/pages/writing.js
@@ -6,25 +6,31 @@ import { GatsbyImage } from "gatsby-plugin-image"
 const WritingPage = () => {
   const data = useStaticQuery(graphql`
     {
-      contentfulAboutPage {
-        id
-        title
-        singleImage {
-          gatsbyImageData
-        }
-        singleImage2 {
-          gatsbyImageData
-        }
-        body {
-          raw
+      allContentfulWriting {
+        edges {
+          node {
+            title
+            date
+            publication
+            slug
+          }
         }
       }
     }
   `)
+
+  let articles = data.allContentfulWriting.edges
+
   return (
     <Layout>
       <div className="container-fluid">
         <h1 className="display-2">Writing</h1>
+        {console.log(articles)}
+        {articles.map(({ node }) => (
+          <li>{node.title}</li>
+        ))}
+
+        {console.log(data)}
       </div>
     </Layout>
   )
