3503ed2e2932b02bb510b8f393b0997c749ba8c2
Jennifer Chow <jennego@gmail.com>
2022-10-26 20:07:02 -0700

    it works now

--- src/pages/writing.js
+++ src/pages/writing.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
-import { useStaticQuery, graphql } from "gatsby"
-import { GatsbyImage } from "gatsby-plugin-image"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import { ListGroup, ListGroupItem } from "reactstrap"
 
 const WritingPage = () => {
   const data = useStaticQuery(graphql`
@@ -26,9 +26,13 @@ const WritingPage = () => {
       <div className="container-fluid">
         <h1 className="display-2">Writing</h1>
         {console.log(articles)}
-        {articles.map(({ node }) => (
-          <li>{node.title}</li>
-        ))}
+        <ul className="writing-list">
+          {articles.map(({ node }) => (
+            <li>
+              <Link to={`/writing/${node.slug}`}>{node.title}</Link>
+            </li>
+          ))}
+        </ul>
 
         {console.log(data)}
       </div>
