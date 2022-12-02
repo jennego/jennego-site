1f6fbbd84608f30fe238645b05571e0ada139906
Jennifer Chow <jennego@gmail.com>
2022-12-01 23:22:35 -0800

    update to gatsby 5

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,36 +1,36 @@
-import React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
+// import React from "react"
+// import { Link, useStaticQuery, graphql } from "gatsby"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { GatsbyImage } from "gatsby-plugin-image";
+// import Layout from "../components/layout"
+// import SEO from "../components/seo"
+// import { GatsbyImage } from "gatsby-plugin-image";
 
-const SecondPage = () => {
-  const data = useStaticQuery(graphql`{
-  file(name: {eq: "backyardsky"}) {
-    name
-    childImageSharp {
-      gatsbyImageData(layout: FULL_WIDTH)
-      fixed {
-        ...GatsbyImageSharpFixed
-      }
-    }
-  }
-}
-`)
-  return (
-    <Layout>
-      <SEO title="Page two" />
-      <h1>Hello from the second page</h1>
-      <p>Welcome to page 2</p>
-      <Link to="/">Go back to the homepage</Link>
-      <div style={{ maxWidth: "800px" }}>
-        <GatsbyImage image={data.file.childImageSharp.gatsbyImageData} className="img-fluid" />
-      </div>
-      <h2>Fixed</h2>
-      <GatsbyImage image={data.file.childImageSharp.gatsbyImageData} />
-    </Layout>
-  );
-}
+// const SecondPage = () => {
+//   const data = useStaticQuery(graphql`{
+//   file(name: {eq: "backyardsky"}) {
+//     name
+//     childImageSharp {
+//       gatsbyImageData(layout: FULL_WIDTH)
+//       fixed {
+//         ...GatsbyImageSharpFixed
+//       }
+//     }
+//   }
+// }
+// `)
+//   return (
+//     <Layout>
+//       <SEO title="Page two" />
+//       <h1>Hello from the second page</h1>
+//       <p>Welcome to page 2</p>
+//       <Link to="/">Go back to the homepage</Link>
+//       <div style={{ maxWidth: "800px" }}>
+//         <GatsbyImage image={data.file.childImageSharp.gatsbyImageData} className="img-fluid" />
+//       </div>
+//       <h2>Fixed</h2>
+//       <GatsbyImage image={data.file.childImageSharp.gatsbyImageData} />
+//     </Layout>
+//   );
+// }
 
-export default SecondPage
+// export default SecondPage
